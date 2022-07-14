d9d3794ef78fbe7abb9226f56cfa04265e263d9d
Gktech1 <favourblessing@gmail.com>
2022-07-14 18:17:14 +0100

    Add CustomerOnboarding

---
+++ services/CustomerOnboarding/CustomerOnboarding.Core/Dto/Gold.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOnboarding.Core.Dto
+{
+    public class GoldResponse
+    {
+        public Gold result { get; set; }
+        public string errorMessage { get; set; }
+        public string[] errorMessages { get; set; }
+        public bool hasError { get; set; }
+        public DateTime timeGenerated { get; set; }
+    }
+
+    public class Gold
+    {
+        public decimal gold { get; set; }
+        public decimal silver { get; set; }
+    }
+
+}
